437eff497310ba3a2dc0d71e9cecf5ecb7c4cd41
Alsobac <luckybhutto975@gmail.com>
2024-11-05 09:16:37 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Maher_Zubair,
+    default: CYBER-ALI,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
